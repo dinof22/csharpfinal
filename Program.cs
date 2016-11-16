4a15a534dbc3c4d302d4e956952c726dd61c960b
Jacob Lofgren <jakeshmearlof4@gmail.com>
2016-11-16 11:49:41 -0700

    first stuff

--- Program.cs
+++ Program.cs
@@ -7,6 +7,25 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            MyFunction();
+            myInt = IntFunction(22);
+            string myString = StringFunction(Console.ReadLine());
+            Console.WriteLine(myString);
+        }
+
+        public static int myInt = 10;
+        
+        public static void MyFunction () {
+            Console.WriteLine("Run Function");
+        }
+
+        public static int IntFunction (int _param) {
+            return _param + myInt; 
+        }
+
+
+        public static string StringFunction (string _name) {
+            return "you did well " + _name;
         }
     }
 }
